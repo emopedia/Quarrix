809fb4d85cbc63c359ff47b377ed5625dbd09970
emopedia <76584247+emopedia@users.noreply.github.com>
2023-01-21 01:06:28 +1300

    Delete src/main directory

--- src/main/java/io/github/quarrix/misc/ColorTranslate.java
+++
@@ -1,9 +0,0 @@
-package io.github.quarrix.misc;
-
-import org.bukkit.ChatColor;
-
-public class ColorTranslate {
-    public static String translate(String s) {
-        return ChatColor.translateAlternateColorCodes('&', s);
-    }
-}
